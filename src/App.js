0746181ff9e5f3363955d9b42f3eb3d9dfcceab5
MariaRosenholm <maria.rosenholm@gmail.com>
2021-11-23 14:36:09 +0200

    styling and search added

--- src/App.js
+++ src/App.js
@@ -5,28 +5,61 @@ import Card from "./components/card";
 class App extends Component {
   state = {
     data: [],
+    search: "",
+    loading: true,
   };
 
   componentDidMount() {
-    axios.get("https://restcountries.com/v3.1/all").then((res) => {
-      this.setState({ data: res.data });
-    });
+    axios
+      .get(
+        "https://restcountries.com/v2/all?fields=name,capital,currencies,flag,population,currency,languages"
+      )
+      .then((res) => {
+        this.setState({ data: res.data, loading: false });
+      });
   }
 
+  searchHandler = (e) => {
+    this.setState({ search: e.target.value });
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header"></header>
-        {this.state.data.map((c) => (
-          <Card
-            name={c.name.common}
-            capital={c.capital}
-            flag={c.flags.png}
-            population={c.population}
+    const countryFilter = this.state.data.filter((country) => {
+      return country.name
+        .toLocaleLowerCase()
+        .includes(this.state.search.toLocaleLowerCase());
+    });
+
+    if (this.state.loading) {
+      return <div className="loader"></div>;
+    }
+
+    if (!this.state.loading) {
+      return (
+        <div className="App">
+          <header className="App-header"></header>
+          <h1>Country search</h1>
+          <input
+            type="text"
+            name="search"
+            placeholder="search by country name"
+            onChange={this.searchHandler}
           />
-        ))}
-      </div>
-    );
+          <div id="countriesWrapper">
+            {countryFilter.map((c) => (
+              <Card
+                name={c.name}
+                capital={c.capital}
+                flag={c.flag}
+                population={c.population}
+                currencies={c.currencies}
+                languages={c.languages}
+              />
+            ))}
+          </div>
+        </div>
+      );
+    }
   }
 }
 
--- src/components/card.js
+++ src/components/card.js
@@ -1,12 +1,27 @@
 import React from "react";
+import numberF from "easy-number-formatter";
 
-const Card = ({ name, capital, flag, population }) => {
+const Card = ({ name, capital, flag, population, currencies, languages }) => {
   return (
     <div className="card">
-      <img src={flag} />
+      <img alt="country flag" src={flag} />
       <h1>{name}</h1>
-      <h2>{capital}</h2>
-      <p>Population: {population}</p>
+      <h2>Capital: {capital}</h2>
+      <h2>Population: {numberF.formatNumber(population)}</h2>
+      <p>
+        Language(s):
+        {languages.map((l, i) => (
+          <span key={i}> {l.name}- </span>
+        ))}
+      </p>
+      <p>
+        Currency(cies):
+        {currencies.map((c) => (
+          <span>
+            {c.symbol} {c.name}-
+          </span>
+        ))}
+      </p>
     </div>
   );
 };
